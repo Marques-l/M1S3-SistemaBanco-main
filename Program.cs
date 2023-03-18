edd666589066a559d6692e581a38bcd1b671e8f8
larissa <larissamarques.ufam@gmail.com>
2023-03-18 05:47:30 -0300

    Limpando.

--- Program.cs
+++ Program.cs
@@ -4,22 +4,6 @@ using M1S3_SistemaBanco;
 ClienteService  _clienteService = new ClienteService(); 
 TicketService _ticketeService = new TicketService(); 
 
-//clientes.Add(new Cliente("Vitor", "123456", "vitor@email","123", "rua", // 'Ocultando isso por enquando, era só exemplo de qualquer
-                                                                            // forma'
-       //      new DateTime(2002, 5, 15), 1));
-            
-//clientes.Add(new Cliente("Fernando", "123456", "Fernando@email","123", "rua",// Ia passar um vazio mas já tem um de mesmo nome com 
-       //      new DateTime(2002, 5, 15), 2));                                 // a lista que pode ficar bagunçado com o return. 
-            
-//clientes.Add(new Cliente("Vanessa", "123456", "Vanessa@email","123", "rua",
-        //     new DateTime(2002, 5, 15), 3));
-           
-
-///clientes.Add(new Cliente("Lucas", "123456", "Lucas@email","123", "rua",
- //           new DateTime(2002, 5, 15), 4));
-
-
-
 string opcao;
 do{
   Console.WriteLine("Bem vindos ao Banco FULL STACK BANCK, escolha uma opção");
